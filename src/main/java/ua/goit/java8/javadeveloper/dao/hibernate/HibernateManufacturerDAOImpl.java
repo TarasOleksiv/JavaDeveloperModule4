bf737297b5c3883f2fe65ca29ec6050472f70167
TarasOleksiv <toleksiv69@gmail.com>
2017-11-30 11:59:15 +0200

    Implement model and hibernate.

---
+++ src/main/java/ua/goit/java8/javadeveloper/dao/hibernate/HibernateManufacturerDAOImpl.java
@@ -0,0 +1,7 @@
+package ua.goit.java8.javadeveloper.dao.hibernate;
+
+/**
+ * Created by t.oleksiv on 30/11/2017.
+ */
+public class HibernateManufacturerDAOImpl {
+}
---
+++ src/main/java/ua/goit/java8/javadeveloper/model/Product.java
@@ -0,0 +1,7 @@
+package ua.goit.java8.javadeveloper.model;
+
+/**
+ * Created by t.oleksiv on 30/11/2017.
+ */
+public class Product {
+}
